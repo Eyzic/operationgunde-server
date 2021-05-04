4d3e63b0c10bd206d3400842f87a30390ba37706
sattarakbari <sattara@student.chalmers.se>
2021-05-04 12:47:07 +0200

    (Y)

--- machinelearning/optimering.py
+++ machinelearning/optimering.py
@@ -8,3 +8,25 @@ from tensorflow.keras.layers import LSTM,Dense,Dropout
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+
+# Load model general and individual
+ind_model = keras.models.load_model('filepath')
+general_model =  keras.models.load_model('filepath')
+
+
+# load the data set and make  data sets compatible to the RNN
+
+# Make a list of all possible data sets for intensity  1-10
+list_datasets = []
+current_predicted_HRV = 0
+optimized_intensity=0
+for dataset in list:
+    #Predict HRV
+    predicted_HRV = ind_model.predict(dataset)
+    if predicted_HRV > current_predicted_HRV:
+        current_predicted_HRV =predicted_HRV
+        optimized_intensity = [] # Intensity used for the dataset
+
+
+
+
--- machinelearning/rnn_individuell.py
+++ machinelearning/rnn_individuell.py
@@ -1,15 +1,27 @@
 import pandas as pd
+
 import numpy as np
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers import LSTM,Dense,Dropout
-
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-class individual_rnn:
-    def __init__(self,ind_rnn_model,input_dataset,output_dataset):
-        self.ind_rnn_model = ind_rnn_model
-        self.input_dataset = input_dataset
-        self.output_dataset = output_dataset
-        1
+from keras.models import load_model
+
+import math
+
+# Load  individual models
+model = load_model('path')
+dataset = [] # Current data set compatible
+X = []
+Y = []
+# train models
+model.fit(X,Y,batch_size=1,epochs=20)
+
+# Save trained models
+keras.models.save_model('path') #
+
+
+
