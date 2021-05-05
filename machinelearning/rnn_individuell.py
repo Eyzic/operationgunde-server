70ae237767cbf28040167c7a99b7d81282122685
sattarakbari <sattara@student.chalmers.se>
2021-05-06 01:40:12 +0200

    Modellerna uppladdad

--- machinelearning/rnn_individuell.py
+++ machinelearning/rnn_individuell.py
@@ -1,19 +1,12 @@
-import pandas as pd
 
-import numpy as np
-
-import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.layers import LSTM,Dense,Dropout
-from sklearn import preprocessing
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from keras.models import load_model
 
-import math
 
-def train_general_model(model,X_train,Y_train):
+def train_general_model(model_path,X_train,Y_train):
+    model = keras.models.load_model(model_path)
     model.fit(X_train,Y_train,epochs = 100,batch_size=1)
     return model
-def predict_hrv_by_general_RNN_model (general_model,X):
-    return general_model.predict(X)
+def predict_hrv_by_general_RNN_model (general_model_path,X):
+    model = keras.models.load_model(general_model_path)
+    return model.predict(X)
+
